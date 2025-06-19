0d1332c0aa7c66dcd6f7a022cb2543ce21eacada
Indu Bala <indu.b@vineforce.net>
2025-06-19 10:53:05 +0530

    Added new ProductDto

--- Project2144/10.2.0/aspnet-core/src/Project2144.Application/Products/Dto/ProductDto.cs
+++ Project2144/10.2.0/aspnet-core/src/Project2144.Application/Products/Dto/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Abp.Application.Services.Dto;
+using Abp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace Project2144.Products.Dto
 {
-    public class ProductDto
+    public class ProductDto : EntityDto<int>
     {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+    public class CreateUpdateProductDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
